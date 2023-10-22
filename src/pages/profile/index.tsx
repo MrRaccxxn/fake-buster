9e0db99d0cfc3d9c49f4681d659146820f7fb954
Ayrton Paredes <ayrton@thinkanddev.com>
2023-10-22 12:00:45 -0400

    profile setting

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -16,8 +16,6 @@ const UserDetailPage: NextPage = () => {
     if (data) setQrCodeData(JSON.parse(data));
   };
 
-  console.log(qrCodeData)
-
   return (
     <>
       <main className="profile-page">
